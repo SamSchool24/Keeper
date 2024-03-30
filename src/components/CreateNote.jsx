d1f8a3c77674e01eb25c8323a126a1de3de0ce68
SamSchool24 <165440203+SamSchool24@users.noreply.github.com>
2024-03-29 20:58:33 -0400

    Update CreateNote.jsx

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -8,7 +8,11 @@ function CreateNote({addNote}) {
       <div className="create-note">
         <input className="create-note-input" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"/>
         <input className="create-note-input" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Take a note..."/>
-        <button onClick={() => addNote(title, content)} className="create-note-button">Add</button>
+        <button onClick={() => { 
+          addNote(title, content);
+          setTitle('');
+          setContent('');
+        }} className="create-note-button">Add</button>
       </div>
     );
   }
